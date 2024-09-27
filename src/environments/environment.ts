074a1b65c44fa296f69ed0a32a98778575e507a3
Đoàn Trọng Bình <binhdt@harmony-at.vn>
2024-09-27 13:25:02 +0700

    add comment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,9 @@ export const environment = {
     production: false,
     msalConfig: {
         auth: {
-            clientId: '24ea6825-5aa9-49fa-bcea-9711d3f5975c',
-            authority: 'https://login.microsoftonline.com/929a6462-3b18-4913-821d-21abf669c438',
-            redirectUri: 'http://localhost:4200',
+            clientId: '24ea6825-5aa9-49fa-bcea-9711d3f5975c', // Application (client) ID
+            authority: 'https://login.microsoftonline.com/929a6462-3b18-4913-821d-21abf669c438', // Directory (tenant) ID
+            redirectUri: 'http://localhost:4200', // RedirectURI
         }
     },
     apiConfig: {
